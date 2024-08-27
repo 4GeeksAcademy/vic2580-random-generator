6de34e916966953632ddbe1f41dfacd7b1eb4ecc
Vic2580 <115164142+Vic2580@users.noreply.github.com>
2024-08-27 00:48:01 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -4,8 +4,56 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/cars.jpg";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", function() {
+    generateCar("button");
+  });
+};
+
+const generateCar = () => {
+  let brand = [
+    "Lamborghini",
+    "Ferrari",
+    "Bugatti",
+    "Porsche",
+    "Rolls-Royce",
+    "Maserati"
+  ];
+  let model = ["Yaris", "Corolla", "Forte", "Sentra", "Civic", "Palio"];
+  let version = [
+    "Concept",
+    "Sport",
+    "Hatchback",
+    "Premium",
+    "Coupe",
+    "Electric"
+  ];
+  let year = ["2002", "2026", "1997", "1976", "2031", "2010"];
+
+  for (let i = 0; i < brand.length; i++) {}
+
+  for (let z = 0; z < model.length; z++) {}
+
+  for (let y = 0; y < version.length; y++) {}
+
+  for (let x = 0; x < year.length; x++) {}
+
+  let brandIndex = Math.floor(Math.random() * brand.length);
+  let modelIndex = Math.floor(Math.random() * model.length);
+  let versionIndex = Math.floor(Math.random() * version.length);
+  let yearIndex = Math.floor(Math.random() * year.length);
+
+  let fullCar =
+    brand[brandIndex] +
+    " " +
+    model[modelIndex] +
+    " " +
+    version[versionIndex] +
+    " " +
+    year[yearIndex];
+  document.querySelector(".yourCar").innerHTML = fullCar;
+
+  return generateCar;
 };
